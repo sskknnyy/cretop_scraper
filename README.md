4a1be50a6193e929a140c0c74f9c8bd25ae7de71
zoe <photozoe1@gmail.com>
2019-07-23 15:00:28 +0900

    cretop crawler add

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Cretop Scraper
 
 투자 검토 회사 리스트를 조회하여 필요한 항목을 엑셀로 출력
+
+## 실행환경
+- Windows 10
+- Internet Explorer
+- Python 3.8
+  - selenium
+  - configparser
--- cretop_scraper.py
+++ cretop_scraper.py
@@ -1,22 +1,66 @@
-#!/usr/bin/python3
+#-*- coding:utf-8 -*-
 
 from selenium import webdriver
+import configparser
+import os
 
-## Settings
-cretop_id = ""
-cretop_pw = ""
 
-## Driver Load
-## Chrome Test
-# driver = webdriver.Chrome('./chromedriver')
-driver = webdriver.Ie('./chromedriver')
-driver.implicitly_wait(3)
+def config_init():
+    cur_path = os.path.abspath(os.path.dirname(__file__))
+    conf_path = os.path.join(cur_path, "config\info.conf")
+    config = configparser.ConfigParser()
+    config.read(conf_path, 'utf-8')
+    return config
 
-## Page Connect
-driver.get('https://www.cretop.com')
 
-## Login Sessing
-driver.find_element_by_id('in_id').send_keys(cretop_id)
-driver.find_element_by_id('in_pw').send_keys(cretop_pw)
+def load_driver(config):
+    driver = webdriver.Ie(config['WEBDRIVER']['webdriver_path'])
+    driver.implicitly_wait(3)
+    return driver
 
-driver.find_element_by_id('loginBtn1').click()
+
+def page_connect(driver, config):
+    driver.get(config['CRETOP']['cretop_url'])
+
+
+def main():
+    config = config_init()
+    driver = load_driver(config)
+    page_connect(driver, config)
+
+    ## Login Session
+    driver.find_element_by_id('in_id').send_keys(config['CRETOP']['cretop_id'])
+    driver.find_element_by_id('in_pw').send_keys(config['CRETOP']['cretop_pw'])
+
+    driver.find_element_by_id('loginBtn1').click()
+
+    ## personal authentification - 필요없을 수 있음
+    # driver.find_element_by_name('indvInfoColAgrYn').click()
+    # driver.find_element_by_name('prpIdInfoAgrYn').click()
+    #
+    # driver.find_element_by_name('name').send_keys(config['CRETOP']['cretop_name'])
+    # driver.find_element_by_name('pid1').send_keys(config['CRETOP']['cretop_pid1'])
+    # driver.find_element_by_name('pid2').send_keys(config['CRETOP']['cretop_pid2'])
+    #
+    # driver.find_element_by_id('CMMBR02R1').click()
+
+    ## 문자 인증 - 일단 skip
+
+    ## 로그인 세션 연장
+
+
+    html = driver.page_source
+    print(html)
+
+    sleep(100)
+    driver.find_element_by_class_name('loginPxad').click()
+    ## 정보를 가져올 회사 리스트 조회
+    ## 고민 대상 - 검색할 대상 회사의 범위를 어떤 기준으로 가져올 것인지....
+    ## 고민 대상 - 검색 도중 세션이 끊길 수 있음. 재 연결하고 긁어오던 부분부터 가져올 방법 고민
+
+    ## 페이지 순회
+
+    ## 엑셀로 파일 저장
+
+if __name__ == '__main__':
+    main()
